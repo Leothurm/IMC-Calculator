d61c1ee09dd19942e278a8f7c899e1aa7160eab6
Leothurm <leonardo.thurm@gmail.com>
2023-01-26 19:34:20 -0300

    javascript finalizado

--- script.js
+++ script.js
@@ -27,7 +27,37 @@ function calculateIMC(){
     } else if(female.checked){
         arrayPerson.push("female")
     }
-    console.log(arrayPerson)
+
+    let imc = arrayPerson[2]/ ((arrayPerson[1])*arrayPerson[1]/10000)
+    
+    let result ='';
+    if(imc <18.5){
+        result = 'Magreza';
+    } else if(imc<=24.9){
+        result = 'Normal';
+    }else if(imc<=30){
+        result = 'Obeso'
+    } else{
+        result = 'Obesidade grave'
+    }
+
+    let h1 = document.createElement('h1')
+    let h2 = document.createElement('h2')
+
+    let t = document.createTextNode(result)
+    let b = document.createTextNode('IMC: ')
+    let r = document.createTextNode(parseFloat(imc).toFixed(2)+ ' kg/m²')
+    
+    h1.appendChild(t)
+    h2.appendChild(b)
+    h2.appendChild(r)
+
+    document.body.appendChild(h1)
+    document.body.appendChild(h2)
+
+    document.getElementById("submit").removeEventListener("click", calculateIMC)
+    document.getElementById("submit").removeEventListener("click", validateForm)
 
 }
+document.getElementById("submit").addEventListener("click", calculateIMC)
 
